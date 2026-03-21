de6061272dce7e20641c7e4ed6da11a0eba0fb76
Andre Fabiano <andregtalima@gmail.com>
2026-03-20 21:23:59 -0300

    update file

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,7 +2,7 @@ import kagglegub
 import os
 import pandas as pd
 
-def load_injures_data():
+def load_injuries_data():
   path = kagglehub.dataset_download("sananmuzaffarov/european-football-injuries-2020-2025")
   file_name = [f for f in os.listdir(path) if f.endswith(".csv")][0]
   file_path = os.path.join(path, file_name)
